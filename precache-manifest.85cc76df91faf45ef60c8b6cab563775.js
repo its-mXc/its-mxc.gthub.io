6973abc9fe4f4582a04fe1dc66488c5281c91201
Uday Nayak <udaynayakgkv@gmail.com>
2020-06-11 19:13:34 +0530

    Updates

---
+++ precache-manifest.85cc76df91faf45ef60c8b6cab563775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "710007e678480ac6e4e0",
+    "url": "/react-portfolio/static/css/main.dbaffbee.chunk.css"
+  },
+  {
+    "revision": "710007e678480ac6e4e0",
+    "url": "/react-portfolio/static/js/main.710007e6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "fdfb9bf68f9236068a8e64b692f4c0fd",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9ba64b97d21ae50be3a41ef603e6445.js"
+  "/react-portfolio/precache-manifest.85cc76df91faf45ef60c8b6cab563775.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
