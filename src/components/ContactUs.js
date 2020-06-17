e587e281de08b8ed15865e50a4afcc3dcff55655
Uday Nayak <udaynayakgkv@gmail.com>
2020-06-17 13:10:48 +0530

    mobile view

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,20 +4,14 @@ export default class ContactUs extends Component {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
-              <center>
-              <a href={`mailto:${resumeData.email}`}><i class="fa fa-3x fa-envelope email-fa"></i></a>
-              <br></br>
-              <br></br>
-              <h3>CONTACT ME WITH EMAIL</h3>
-              <br></br>
-              <a href={`mailto:${resumeData.email}`}>{resumeData.email}</a>
-              </center>
-              </p>
-            </div>
-          </div>
+          <center>
+          <a href={`mailto:${resumeData.email}`}><i class="fa fa-4x fa-envelope email-fa"></i></a>
+          <br></br>
+          <br></br>
+          <h3>CONTACT ME WITH EMAIL</h3>
+          <br></br>
+          <a href={`mailto:${resumeData.email}`}>{resumeData.email}</a>
+          </center>
         </section>
         );
   }
